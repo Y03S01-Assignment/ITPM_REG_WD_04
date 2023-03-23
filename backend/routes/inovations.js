f193a1f888d3ef0c2cb6f583a0f95c54261fb88f
Vinura de Silva <m.v.r.desilva@gmail.com>
2023-03-24 00:01:47 +0530

    Update inovations.js

--- backend/routes/inovations.js
+++ backend/routes/inovations.js
@@ -21,4 +21,48 @@ router.post('/innovation/save', (req,res)=>{
     
 });
 
+router.get('/innovation', (req,res) =>{
+    Innovations.find().exec((err, innovations) => {
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:true,
+            existingInnovations:innovations
+        });
+    });
+});
+
+router.put('/innovation/update/:id', (req,res) =>{
+    Innovations.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err, innovation) => {
+            if(err){
+                return res.status(400).json({error:err});
+            }
+
+            return res.status(200).json({
+                success:"Updated Successfully"
+            });
+        }
+    );
+});
+
+router.delete('/innovation/delete/:id', (req,res) =>{
+    Innovations.findByIdAndRemove(req.params.id).exec((err, deletedInnovation) => {
+        if(err) return res.status(400).json({
+            message:"Delete Unsuccessful",err
+        });
+        return res.json({
+            message:"Delete Successful",deletedInnovation
+        });
+    });
+});
+
+
 module.exports = router;
