a4c813a9b32a1323d874cc205d47f98364168ade
Vinura de Silva <m.v.r.desilva@gmail.com>
2023-03-23 23:45:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
