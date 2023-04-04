76b83e349a5b99307bf775892e845534c0a6eb32
Vinura de Silva <m.v.r.desilva@gmail.com>
2023-04-04 10:28:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import React from "react"; 
-import axios from "axios";
-import { Component } from "react";
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
-export default class App extends Component {
-  constructor(props) {  
-    super(props);
-    this.state = {
-      innovation: [],
-    };  
-
-  }  
-
-  
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Dashboard
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />} />
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Tom</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            Welcome to your dashboard home page!
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design Pro ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
 }
+
+export default App;
